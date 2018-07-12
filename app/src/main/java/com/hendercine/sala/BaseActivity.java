616984a6c8b2f88a0346ffe0144daf1ec3244ff2
hendercine <james@hendercine.com>
2018-07-12 13:48:37 -0700

    feat($BaseActivity): Add Timber log tree
    * Override onCreate()

--- app/src/main/java/com/hendercine/sala/BaseActivity.java
+++ app/src/main/java/com/hendercine/sala/BaseActivity.java
@@ -1,10 +1,28 @@
+/*
+ * Created by James Henderson on 2018
+ * Copyright (c) Hendercine Productions and James Henderson 2018.
+ * All rights reserved.
+ *
+ * Last modified 7/12/18 1:08 PM
+ */
+
 package com.hendercine.sala;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import timber.log.Timber;
 
 /**
  * SundayAssemblyLosAngeles-1.1 created by hendercine on 7/12/18.
  */
-public class BaseActivity extends AppCompatActivity {
+public abstract class BaseActivity extends AppCompatActivity {
 
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (BuildConfig.DEBUG) {
+            Timber.plant(new Timber.DebugTree());
+        }
+    }
 }
