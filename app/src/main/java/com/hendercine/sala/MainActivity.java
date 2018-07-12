c210410ae7f6a9a77f6a023d0c0265af0bd56a7e
hendercine <james@hendercine.com>
2018-07-12 15:16:57 -0700

    refactor: Extend MainActivity with BaseActivity

--- app/src/main/java/com/hendercine/sala/MainActivity.java
+++ app/src/main/java/com/hendercine/sala/MainActivity.java
@@ -1,9 +1,8 @@
 package com.hendercine.sala;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends BaseActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
