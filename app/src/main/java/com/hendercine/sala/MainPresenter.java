37c76374a95653df7d74a4da80a33e1abeb39be9
hendercine <james@hendercine.com>
2018-07-12 16:48:33 -0700

    feat: Create MainPresenter class

---
+++ app/src/main/java/com/hendercine/sala/MainPresenter.java
@@ -0,0 +1,28 @@
+/*
+ * Created by James Henderson on 2018
+ * Copyright (c) Hendercine Productions and James Henderson 2018.
+ * All rights reserved.
+ *
+ * Last modified 7/12/18 4:42 PM
+ */
+
+package com.hendercine.sala;
+
+import java.util.Random;
+/**
+ * SundayAssemblyLosAngeles-1.1 created by hendercine on 7/12/18.
+ */
+public class MainPresenter extends BasePresenter<MainContract.View>
+        implements MainContract.Presenter {
+
+    private String[] helloTexts = {"BONJOUR", "HOLA", "HALLO", "MERHABA",
+            "HELLO", "CIAO", "KONNICHIWA"};
+
+    @Override
+    public void loadHelloText() {
+        Random random = new Random();
+        String hello = helloTexts[random.nextInt(helloTexts.length)];
+        getView().onTextLoaded(hello);
+
+    }
+}
