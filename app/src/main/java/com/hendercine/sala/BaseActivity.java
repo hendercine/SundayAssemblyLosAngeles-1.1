4fce452cb3f08017a58fb32520a9eb83956a9139
hendercine <james@hendercine.com>
2018-07-12 13:11:37 -0700

    feat($src): Add BaseActivity class
    * Extends AppCompatActivity

---
+++ app/src/main/java/com/hendercine/sala/BaseActivity.java
@@ -0,0 +1,10 @@
+package com.hendercine.sala;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * SundayAssemblyLosAngeles-1.1 created by hendercine on 7/12/18.
+ */
+public class BaseActivity extends AppCompatActivity {
+
+}
