73a8d7d43b6e807f74301408a86769aad8b75bd2
hendercine <james@hendercine.com>
2018-07-12 17:07:47 -0700

    feat: Modify MainActivity to show dummy content

--- app/src/main/java/com/hendercine/sala/MainActivity.java
+++ app/src/main/java/com/hendercine/sala/MainActivity.java
@@ -1,12 +1,47 @@
 package com.hendercine.sala;
 
 import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.View;
+import android.widget.TextView;
 
-public class MainActivity extends BaseActivity {
+import butterknife.BindView;
+
+public class MainActivity extends BaseActivity implements MainContract.View,
+                                                          View.OnClickListener{
+
+    @BindView(R.id.tvHello)
+    TextView mTextView;
+    private MainPresenter mPresenter;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+    protected int getContentResource() {
+        return R.layout.activity_main;
     }
+
+    @Override
+    protected void init(@Nullable Bundle savedInstanceState) {
+        mTextView.setOnClickListener(this);
+        mPresenter = new MainPresenter();
+        mPresenter.attach(this);
+        mPresenter.loadHelloText();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        mPresenter.detach();
+    }
+
+    @Override
+    public void onTextLoaded(String text) {
+        mTextView.setText(text);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        mPresenter.loadHelloText();
+    }
+
 }
