0a4405b63e082ef686b7b3b205562625c9e9e3be
hendercine <james@hendercine.com>
2018-07-12 17:05:31 -0700

    feat($BaseActivity): Modify to set dummy features

--- app/src/main/java/com/hendercine/sala/BaseActivity.java
+++ app/src/main/java/com/hendercine/sala/BaseActivity.java
@@ -12,6 +12,7 @@ import android.content.Context;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.os.Bundle;
+import android.support.annotation.LayoutRes;
 import android.support.annotation.Nullable;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
@@ -30,10 +31,17 @@ public abstract class BaseActivity extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(getContentResource());
+        init(savedInstanceState);
         if (BuildConfig.DEBUG) {
             Timber.plant(new Timber.DebugTree());
         }
     }
+
+    @LayoutRes
+    protected abstract int getContentResource();
+
+    protected abstract void init(@Nullable Bundle savedInstanceState);
 
     @Override
     protected void onDestroy() {
