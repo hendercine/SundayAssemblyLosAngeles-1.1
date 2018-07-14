66a99050ecb432631ad223a1321fea91dffd64b7
hendercine <james@hendercine.com>
2018-07-13 17:23:13 -0700

    feat: Create ActivityModule

---
+++ app/src/main/java/com/hendercine/sala/di/module/ActivityModule.java
@@ -0,0 +1,38 @@
+package com.hendercine.sala.di.module;
+
+import android.content.Context;
+
+import com.hendercine.sala.ChromeTabsWrapper;
+import com.hendercine.sala.main.MainContract;
+import com.hendercine.sala.main.MainPresenter;
+
+import javax.inject.Singleton;
+
+import dagger.Module;
+import dagger.Provides;
+
+/**
+ * Created by James Henderson on 7/13/18.
+ */
+
+@Module
+public class ActivityModule {
+
+    private Context mContext;
+
+    public ActivityModule(Context context) {
+        this.mContext = context;
+    }
+
+    @Provides
+    @Singleton
+    ChromeTabsWrapper providesChromeTabsWrapper() {
+        return new ChromeTabsWrapper(mContext);
+    }
+
+    @Provides
+    @Singleton
+    MainContract.Presenter providesMainPresenter() {
+        return new MainPresenter();
+    }
+}
