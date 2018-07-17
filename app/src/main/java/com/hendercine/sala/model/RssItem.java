b445a39cacf00e5f4e0b26871aa17254091617b0
hendercine <james@hendercine.com>
2018-07-17 12:03:51 -0700

    feat($model): Setup RssItem pojo

--- app/src/main/java/com/hendercine/sala/model/RssItem.java
+++ app/src/main/java/com/hendercine/sala/model/RssItem.java
@@ -6,11 +6,75 @@
  * Last modified 7/17/18 11:49 AM
  */
 
-package com.hendercine.sala;
+package com.hendercine.sala.model;
 
 /**
  * SundayAssemblyLosAngeles-1.1 created by hendercine on 7/17/18.
  */
 public class RssItem {
 
+    private String mTitle;
+    private String mUrl;
+    private String mImageUrl;
+    private String mPubdate;
+
+    private int mFeedId;
+    private String mFeedTitle;
+    private String mDescription;
+
+    public String getTitle() {
+        return mTitle;
+    }
+
+    public void setTitle(String title) {
+        mTitle = title;
+    }
+
+    public String getUrl() {
+        return mUrl;
+    }
+
+    public void setUrl(String url) {
+        mUrl = url;
+    }
+
+    public String getImageUrl() {
+        return mImageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        mImageUrl = imageUrl;
+    }
+
+    public String getPubdate() {
+        return mPubdate;
+    }
+
+    public void setPubdate(String pubdate) {
+        mPubdate = pubdate;
+    }
+
+    public int getFeedId() {
+        return mFeedId;
+    }
+
+    public void setFeedId(int feedId) {
+        mFeedId = feedId;
+    }
+
+    public String getFeedTitle() {
+        return mFeedTitle;
+    }
+
+    public void setFeedTitle(String feedTitle) {
+        mFeedTitle = feedTitle;
+    }
+
+    public String getDescription() {
+        return mDescription;
+    }
+
+    public void setDescription(String description) {
+        mDescription = description;
+    }
 }
