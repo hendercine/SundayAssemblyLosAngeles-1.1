6680c2dfdb5dbc0befa3dc6e4a5d6179ceeaaeaf
hendercine <james@hendercine.com>
2018-07-17 11:42:52 -0700

    feat($session): Create SessionData class

---
+++ app/src/main/java/com/hendercine/sala/session/SessionData.java
@@ -0,0 +1,16 @@
+/*
+ * Created by James Henderson on 2018
+ * Copyright (c) Hendercine Productions and James Henderson 2018.
+ * All rights reserved.
+ *
+ * Last modified 7/17/18 11:41 AM
+ */
+
+package com.hendercine.sala.session;
+
+/**
+ * SundayAssemblyLosAngeles-1.1 created by hendercine on 7/17/18.
+ */
+public class SessionData {
+
+}
