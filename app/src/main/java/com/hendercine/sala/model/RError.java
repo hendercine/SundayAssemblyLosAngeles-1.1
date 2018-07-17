b2d178814ca887ae744779ae916d8766de0acc56
hendercine <james@hendercine.com>
2018-07-17 12:04:30 -0700

    feat($model): Setup RError pojo

--- app/src/main/java/com/hendercine/sala/model/RError.java
+++ app/src/main/java/com/hendercine/sala/model/RError.java
@@ -1,3 +1,11 @@
+/*
+ * Created by James Henderson on 2018
+ * Copyright (c) Hendercine Productions and James Henderson 2018.
+ * All rights reserved.
+ *
+ * Last modified 7/17/18 11:51 AM
+ */
+
 package com.hendercine.sala.model;
 
 /**
@@ -5,4 +13,13 @@ package com.hendercine.sala.model;
  */
 public class RError {
 
+    private final String mMessage;
+
+    public RError(String message) {
+        mMessage = message;
+    }
+
+    public String getMessage() {
+        return mMessage;
+    }
 }
