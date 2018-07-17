a9635205f95c20df30850015a48617db9c98e4f8
hendercine <james@hendercine.com>
2018-07-16 17:26:56 -0700

    feat: Add BaseFragment class

---
+++ app/src/main/java/com/hendercine/sala/base/BaseFragment.java
@@ -0,0 +1,90 @@
+/*
+ * Created by James Henderson on 2018
+ * Copyright (c) Hendercine Productions and James Henderson 2018.
+ * All rights reserved.
+ *
+ * Last modified 7/16/18 5:03 PM
+ */
+
+package com.hendercine.sala.base;
+
+import android.app.Fragment;
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.os.Bundle;
+import android.support.annotation.LayoutRes;
+import android.support.annotation.Nullable;
+import android.support.design.widget.Snackbar;
+import android.widget.Toast;
+
+import javax.inject.Inject;
+
+/**
+ * SundayAssemblyLosAngeles-1.1 created by hendercine on 7/16/18.
+ */
+public abstract class BaseFragment<T extends BaseMvpPresenter> extends Fragment implements BaseView {
+
+    @Inject
+    T mPresenter;
+
+    FragmentComponent mFragmentComponent;
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mFragmentComponent = DaggerFragmentComponent.builder()
+                .fragmentModule(new FragmentModule())
+                .build();
+        injectDependencies();
+    }
+
+    @Override
+    public void onFail(RError error) {
+        if (isAdded()) {
+            showToast(error.getMessage());
+        }
+    }
+
+    public FragmentComponent getFragmentComponent() {
+        return mFragmentComponent;
+    }
+
+    public T getPresenter() {
+        return mPresenter;
+    }
+
+    @LayoutRes
+    protected abstract int getContentResource();
+
+    protected abstract void init(@Nullable Bundle state);
+
+    protected abstract void injectDependencies();
+
+    public void showSnackBar(int resId) {
+        Snackbar.make(
+                getActivity().findViewById(android.R.id.content),
+                resId,
+                Snackbar.LENGTH_SHORT
+        ).show();
+    }
+
+    public void showToast(String msg) {
+        Toast.makeText(getActivity().getApplicationContext(), msg, Toast.LENGTH_SHORT).show();
+    }
+
+    public void showToast(int resId) {
+        Toast.makeText(getActivity().getApplicationContext(), resId, Toast.LENGTH_SHORT).show();
+    }
+
+    public static boolean isNetworkAvailable(Context context) {
+        ConnectivityManager connectivityManager =
+                (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo activeNetwork = null;
+        if (connectivityManager != null) {
+            activeNetwork = connectivityManager.getActiveNetworkInfo();
+        }
+        return activeNetwork != null && activeNetwork.isConnectedOrConnecting();
+    }
+
+}
