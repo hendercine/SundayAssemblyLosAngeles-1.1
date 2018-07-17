5145d246050def74ff7a4deb11e86f23992ea96d
hendercine <james@hendercine.com>
2018-07-17 12:55:26 -0700

    feat($base): Create AsyncCallbackInterface.
    * Add showLoading() and hideLoading() methods

---
+++ app/src/main/java/com/hendercine/sala/base/AsyncCallbackView.java
@@ -0,0 +1,19 @@
+/*
+ * Created by James Henderson on 2018
+ * Copyright (c) Hendercine Productions and James Henderson 2018.
+ * All rights reserved.
+ *
+ * Last modified 7/17/18 12:54 PM
+ */
+
+package com.hendercine.sala.base;
+
+/**
+ * SundayAssemblyLosAngeles-1.1 created by hendercine on 7/17/18.
+ */
+public interface AsyncCallbackView {
+
+    void showLoading();
+
+    void hideLoading();
+}
