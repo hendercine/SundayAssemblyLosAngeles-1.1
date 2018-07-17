28b94e35408ad6a29ed09c6fa28098ba247dec51
hendercine <james@hendercine.com>
2018-07-17 12:57:52 -0700

    feat($rss): Create RssFragmentAdapter class

---
+++ app/src/main/java/com/hendercine/sala/rss/RssFragmentAdapter.java
@@ -0,0 +1,18 @@
+/*
+ * Created by James Henderson on 2018
+ * Copyright (c) Hendercine Productions and James Henderson 2018.
+ * All rights reserved.
+ *
+ * Last modified 7/17/18 12:57 PM
+ */
+
+package com.hendercine.sala.rss;
+
+import android.support.v4.app.FragmentPagerAdapter;
+
+/**
+ * SundayAssemblyLosAngeles-1.1 created by hendercine on 7/17/18.
+ */
+public class RssFragmentAdapter extends FragmentPagerAdapter {
+
+}
