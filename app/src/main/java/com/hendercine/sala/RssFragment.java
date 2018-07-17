2c9ed6fa49ddbbd451f991bc8e220b6d02761a05
hendercine <james@hendercine.com>
2018-07-16 17:27:59 -0700

    feat: Add RssFragment class

---
+++ app/src/main/java/com/hendercine/sala/RssFragment.java
@@ -0,0 +1,18 @@
+/*
+ * Created by James Henderson on 2018
+ * Copyright (c) Hendercine Productions and James Henderson 2018.
+ * All rights reserved.
+ *
+ * Last modified 7/16/18 5:22 PM
+ */
+
+package com.hendercine.sala;
+
+import com.hendercine.sala.base.BaseFragment;
+
+/**
+ * SundayAssemblyLosAngeles-1.1 created by hendercine on 7/16/18.
+ */
+public class RssFragment extends BaseFragment {
+
+}
