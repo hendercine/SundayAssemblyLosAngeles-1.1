effe7280ff5171069c1571fa6ad415790da73cf2
hendercine <james@hendercine.com>
2018-07-17 11:49:39 -0700

    feat($session): Setup SessionData class

--- app/src/main/java/com/hendercine/sala/session/SessionData.java
+++ app/src/main/java/com/hendercine/sala/session/SessionData.java
@@ -8,9 +8,30 @@
 
 package com.hendercine.sala.session;
 
+import java.util.HashMap;
+import java.util.List;
+
+import javax.inject.Inject;
 /**
  * SundayAssemblyLosAngeles-1.1 created by hendercine on 7/17/18.
  */
 public class SessionData {
 
+    @Inject
+    public SessionData() {
+    }
+
+    private final HashMap<String, List<RssItem>> mContentMap = new HashMap<>();
+
+    public boolean hasUrl(String url) {
+        return mContentMap.containsKey(url);
+    }
+
+    public void addContent(String url, List<RssItem> items) {
+        mContentMap.put(url, items);
+    }
+
+    public List<RssItem> getContent(String url) {
+        return mContentMap.get(url);
+    }
 }
