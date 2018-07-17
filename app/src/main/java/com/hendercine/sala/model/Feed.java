070fb46ead0272f9030f0894b530dc2d65bfcb0f
hendercine <james@hendercine.com>
2018-07-17 12:08:45 -0700

    feat($model): Setup Feed pojo

--- app/src/main/java/com/hendercine/sala/model/Feed.java
+++ app/src/main/java/com/hendercine/sala/model/Feed.java
@@ -1,4 +1,14 @@
+/*
+ * Created by James Henderson on 2018
+ * Copyright (c) Hendercine Productions and James Henderson 2018.
+ * All rights reserved.
+ *
+ * Last modified 7/17/18 11:51 AM
+ */
+
 package com.hendercine.sala.model;
+
+import com.google.gson.annotations.SerializedName;
 
 import java.io.Serializable;
 
@@ -7,4 +17,31 @@ import java.io.Serializable;
  */
 public class Feed implements Serializable {
 
+    @SerializedName("i")
+    private Integer mFeedId;
+
+    @SerializedName("n")
+    private String mTitle;
+
+    @SerializedName("l")
+    private String mUrl;
+
+    @SerializedName("e")
+    private String mEncoding;
+
+    public Integer getFeedId() {
+        return mFeedId;
+    }
+
+    public String getTitle() {
+        return mTitle;
+    }
+
+    public String getUrl() {
+        return mUrl;
+    }
+
+    public String getEncoding() {
+        return mEncoding;
+    }
 }
