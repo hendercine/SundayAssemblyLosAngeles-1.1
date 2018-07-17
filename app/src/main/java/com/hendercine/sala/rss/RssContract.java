14864e0b30fb877657a7983789768b747e3d34b5
hendercine <james@hendercine.com>
2018-07-17 11:24:08 -0700

    feat($rss): Setup RssContract interfaces

--- app/src/main/java/com/hendercine/sala/rss/RssContract.java
+++ app/src/main/java/com/hendercine/sala/rss/RssContract.java
@@ -8,9 +8,29 @@
 
 package com.hendercine.sala.rss;
 
+import com.hendercine.sala.base.BaseMvpPresenter;
+import com.hendercine.sala.base.BaseView;
+
+import java.util.List;
 /**
- * SundayAssemblyLosAngeles-1.1 created by hendercine on 7/17/18.
+ * SundayAssemblyLosAngeles-1.1 created by hendercine on 7/17/18. Code source:
+ * https://medium.com/android-bits
+ * /android-app-from-scratch-part-3-implementing-app-logic-2b62ae65dcc4
  */
 public interface RssContract {
+
+    // User actions. Presenter will implement
+    interface Presenter extends BaseMvpPresenter<RssContract.View> {
+
+    }
+
+    // Action callbacks. Activity/Fragment will implement
+    interface View extends BaseView, AsyncCallBackView {
+
+        void onRssItemsLoaded(List<RssItem> rssItems);
+
+        void onBrowse(RssItem rssItem);
+
+    }
 
 }
