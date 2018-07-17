7b1524cefb1e1ecf91cf1012a8ddb94d08304808
hendercine <james@hendercine.com>
2018-07-17 11:51:45 -0700

    feat($model): Create Rss model classes

---
+++ app/src/main/java/com/hendercine/sala/model/Feed.java
@@ -0,0 +1,10 @@
+package com.hendercine.sala.model;
+
+import java.io.Serializable;
+
+/**
+ * SundayAssemblyLosAngeles-1.1 created by hendercine on 7/17/18.
+ */
+public class Feed implements Serializable {
+
+}
---
+++ app/src/main/java/com/hendercine/sala/model/RError.java
@@ -0,0 +1,8 @@
+package com.hendercine.sala.model;
+
+/**
+ * SundayAssemblyLosAngeles-1.1 created by hendercine on 7/17/18.
+ */
+public class RError {
+
+}
