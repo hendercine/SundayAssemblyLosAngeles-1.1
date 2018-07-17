baf09788e8ad8965c77b912010c12082c2ffa09c
hendercine <james@hendercine.com>
2018-07-17 13:07:54 -0700

    feat($component): Create FragmentComponent
    * Add Component annotation  referring to FragmentModule
    * Add inject method to inject RssFragment

---
+++ app/src/main/java/com/hendercine/sala/di/component/FragmentComponent.java
@@ -0,0 +1,24 @@
+/*
+ * Created by James Henderson on 2018
+ * Copyright (c) Hendercine Productions and James Henderson 2018.
+ * All rights reserved.
+ *
+ * Last modified 7/17/18 1:06 PM
+ */
+
+package com.hendercine.sala.di.component;
+
+import com.hendercine.sala.di.module.FragmentModule;
+import com.hendercine.sala.rss.RssFragment;
+
+import dagger.Component;
+/**
+ * SundayAssemblyLosAngeles-1.1 created by hendercine on 7/17/18.
+ */
+
+@Component(modules = {FragmentModule.class})
+public interface FragmentComponent {
+
+    void inject(RssFragment obj);
+
+}
