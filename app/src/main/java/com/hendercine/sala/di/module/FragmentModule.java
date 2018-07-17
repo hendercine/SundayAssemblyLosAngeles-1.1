f5540191a9cd5b62820da95f78cf266aeecae4dd
hendercine <james@hendercine.com>
2018-07-17 12:52:05 -0700

    feat($module): Setup FragmentModule provides

--- app/src/main/java/com/hendercine/sala/di/module/FragmentModule.java
+++ app/src/main/java/com/hendercine/sala/di/module/FragmentModule.java
@@ -1,8 +1,29 @@
+/*
+ * Created by James Henderson on 2018
+ * Copyright (c) Hendercine Productions and James Henderson 2018.
+ * All rights reserved.
+ *
+ * Last modified 7/17/18 12:48 PM
+ */
+
 package com.hendercine.sala.di.module;
 
+import com.hendercine.sala.rss.RssContract;
+import com.hendercine.sala.rss.RssPresenter;
+import com.hendercine.sala.session.SessionData;
+
+import dagger.Module;
+import dagger.Provides;
 /**
  * SundayAssemblyLosAngeles-1.1 created by hendercine on 7/17/18.
  */
+
+@Module
 public class FragmentModule {
+
+    @Provides
+    RssContract.Presenter providesRssPresenter(SessionData sessionData) {
+        return new RssPresenter(sessionData);
+    }
 
 }
