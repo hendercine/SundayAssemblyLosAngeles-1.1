6fcf366f23408a842ccf51f7cf278f6f83078ff6
hendercine <james@hendercine.com>
2018-07-17 11:38:47 -0700

    feat($rss): Setup RssPresenter class

--- app/src/main/java/com/hendercine/sala/rss/RssPresenter.java
+++ app/src/main/java/com/hendercine/sala/rss/RssPresenter.java
@@ -21,4 +21,48 @@ import timber.log.Timber;
  */
 public class RssPresenter extends BasePresenter<RssContract.View> implements
         RssContract.Presenter, OnRssParserListener {
+
+    private SessionData mSessionData;
+
+    @Inject
+    public RssPresenter(SessionData sessionData) {
+        mSessionData = sessionData;
+    }
+
+    @Override
+    public void loadRssItems(Feed feed, boolean fromCache) {
+        if (mSessionData.hasUrl(feed.getUrl()) && fromCache) {
+            Timber.v("Read from cache: " + feed.getUrl());
+
+            getView().onRssItemsLoaded(mSessionData.getContent(feed.getUrl()));
+        } else {
+            RssReader request = new RssReader(this, feed.getUrl());
+            request.execute();
+            getView().showLoading();
+        }
+    }
+
+    @Override
+    public void browseRssUrl(RssItem rssItem) {
+        if (isAttached()) {
+            getView().onBrowse(rssItem);
+        }
+    }
+
+    @Override
+    public void onSuccess(List<RssItem> rssItemList, String rssUrl) {
+        mSessionData.addContent(rssUrl, rssItemList);
+        if (isAttached()) {
+            getView().onRssItemsLoaded(rssItemList);
+            getView().hideLoading();
+        }
+    }
+
+    @Override
+    public void onFail(String rssUrl) {
+        if (isAttached()) {
+            getView().hideLoading();
+            getView().onFail(new RError("Failed to RSS!"));
+        }
+    }
 }
