fa39a0e21a1c13b9e933e5744611029c35a16048
hendercine <james@hendercine.com>
2018-07-22 16:28:11 -0700

    feat($java): Create Application class

---
+++ app/src/main/java/SalaApplication.java
@@ -0,0 +1,10 @@
+package PACKAGE_NAME;
+
+import android.app.Application;
+
+/**
+ * SundayAssemblyLosAngeles-1.1 created by artemis on 7/22/18.
+ */
+public class SalaApplication extends Application {
+
+}
