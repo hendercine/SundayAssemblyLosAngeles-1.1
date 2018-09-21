e8a877bb7dcb1e9c5adc8d3beb83ce177f6cbad7
hendercine <james@hendercine.com>
2018-09-21 15:55:32 -0700

    feat($model): add User POJO

---
+++ app/src/main/java/com/hendercine/sala_v1_1/User.java
@@ -0,0 +1,56 @@
+/*
+ * Created by James Henderson on 2018
+ * Copyright (c) Hendercine Productions and James Henderson 2018.
+ * All rights reserved.
+ *
+ * Last modified 9/21/18 3:54 PM
+ */
+
+package com.hendercine.sala_v1_1;
+
+import com.google.firebase.database.IgnoreExtraProperties;
+/**
+ * SundayAssemblyLosAngeles-1.1 created by artemis on 9/21/18.
+ */
+
+@IgnoreExtraProperties
+public class User {
+
+    public String username;
+    public String email;
+    public String userId;
+
+    public User() {
+        // Default constructor required for calls to DataSnapshot.getValue(User.class)
+    }
+
+    public User(String username, String email, String userId) {
+        this.username = username;
+        this.email = email;
+        this.userId = userId;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+}
