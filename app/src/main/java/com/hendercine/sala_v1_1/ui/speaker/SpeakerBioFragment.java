3cdb65260e3401b8a3c6bfaf53bd6846901009b7
hendercine <james@hendercine.com>
2018-10-24 17:34:34 -0700

    feat($ui): add SpeakerBioFragment & Layout

---
+++ app/src/main/java/com/hendercine/sala_v1_1/ui/speaker/SpeakerBioFragment.java
@@ -0,0 +1,18 @@
+/*
+ * Created by James Henderson on 2018
+ * Copyright (c) Hendercine Productions and James Henderson 2018.
+ * All rights reserved.
+ *
+ * Last modified 10/24/18 4:58 PM
+ */
+
+package com.hendercine.sala_v1_1.ui;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * SundayAssemblyLosAngeles-1.1 created by hendercine on 10/24/18.
+ */
+public class SpeakerBioFragment extends Fragment {
+
+}
