5552e9d12e308b233940a1b57378331e42f13207
hendercine <james@hendercine.com>
2018-10-30 11:06:36 -0700

    feat($speaker): change bg color

--- app/src/main/java/com/hendercine/sala_v1_1/ui/speaker/SpeakerBioFragment.java
+++ app/src/main/java/com/hendercine/sala_v1_1/ui/speaker/SpeakerBioFragment.java
@@ -6,13 +6,25 @@
  * Last modified 10/24/18 4:58 PM
  */
 
-package com.hendercine.sala_v1_1.ui;
+package com.hendercine.sala_v1_1.ui.speaker;
 
-import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.hendercine.sala_v1_1.ui.base.BaseFragment;
 
 /**
  * SundayAssemblyLosAngeles-1.1 created by hendercine on 10/24/18.
  */
-public class SpeakerBioFragment extends Fragment {
+public class SpeakerBioFragment extends BaseFragment {
 
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return super.onCreateView(inflater, container, savedInstanceState);
+    }
 }
