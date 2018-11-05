96de0caee01b60b142895f090debd4598672b7a0
hendercine <james@hendercine.com>
2018-11-05 11:38:00 -0800

    feat($speaker): add data model logic

--- app/src/main/java/com/hendercine/sala_v1_1/ui/speaker/SpeakerBioFragment.java
+++ app/src/main/java/com/hendercine/sala_v1_1/ui/speaker/SpeakerBioFragment.java
@@ -19,6 +19,7 @@ import android.widget.TextView;
 
 import com.bumptech.glide.Glide;
 import com.hendercine.sala_v1_1.R;
+import com.hendercine.sala_v1_1.models.Performer;
 import com.hendercine.sala_v1_1.ui.base.BaseFragment;
 
 import butterknife.BindView;
@@ -28,6 +29,11 @@ import butterknife.ButterKnife;
  * SundayAssemblyLosAngeles-1.1 created by hendercine on 10/24/18.
  */
 public class SpeakerBioFragment extends BaseFragment {
+
+    private Performer mSpeakerData;
+    private String mSpeakerNameText;
+    private String mSpeakerBioText;
+    private String mSpeakerPhotoUrl;
 
     @BindView(R.id.speaker_name_title)
     TextView mSpeakerNameTV;
@@ -43,10 +49,20 @@ public class SpeakerBioFragment extends BaseFragment {
         View rootView = inflater.inflate(getFragmentLayout(), container, false);
         mUnbinder = ButterKnife.bind(this, rootView);
 
-        mSpeakerNameTV.setText(R.string.speaker_name_placeholder);
-        mSpeakerBioTV.setText(R.string.mission_statement);
+        if (mSpeakerData != null) {
+            mSpeakerNameText = mSpeakerData.getPerformerName();
+            mSpeakerBioText = mSpeakerData.getPerformerBio();
+            mSpeakerPhotoUrl = mSpeakerData.getPerformerPicUrl();
+        } else {
+            mSpeakerNameText = String.valueOf(R.string.speaker_name_placeholder);
+            mSpeakerBioText = String.valueOf(R.string.mission_statement);
+            mSpeakerPhotoUrl = String.valueOf(R.drawable.baseline_account_circle_48);
+        }
+
+        mSpeakerNameTV.setText(mSpeakerNameText);
+        mSpeakerBioTV.setText(mSpeakerBioText);
         Glide.with(this)
-                .load(R.drawable.pippa_evans)
+                .load(mSpeakerPhotoUrl)
                 .into(mSpeakerPhotoIV);
 
         return rootView;
